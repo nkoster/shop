eef39ab434b2144c401aff9b746bae9aaaefa37f
Niels <n.koster@portavita.eu>
2023-07-02 00:12:25 +0200

    Make it work

--- components/Product.js
+++ components/Product.js
@@ -1,24 +1,33 @@
-import { useState } from "react";
-
+import { useState } from 'react'
+import { formatCurrencyString, useShoppingCart } from 'use-shopping-cart'
 export default function Product({ product }) {
-  const { name, price, emoji } = product;
-  const [quantity, setQuantity] = useState(1);
+  const { addItem } = useShoppingCart()
+
+  const { name, price, emoji } = product
+  const [quantity, setQuantity] = useState(1)
 
   const decreaseQuantity = () => {
     if (quantity > 1) {
-      setQuantity(quantity - 1);
+      setQuantity(quantity - 1)
     }
-  };
+  }
 
   const increaseQuantity = () => {
-    setQuantity(quantity + 1);
-  };
+    setQuantity(quantity + 1)
+  }
+
+  const addToCart = () => {
+    addItem(product, { count: quantity })
+    setQuantity(1)
+  }
 
   return (
     <article className="flex flex-col gap-3 bg-white p-8 rounded-xl shadow-md text-center mb-6">
       <div className="text-8xl cursor-default">{emoji}</div>
       <div className="text-lg">{name}</div>
-      <div className="text-2xl font-semibold mt-auto">{price}</div>
+      <div className="text-2xl font-semibold mt-auto">
+        {formatCurrencyString({ value: price, currency: 'EUR' })}
+      </div>
       <div className="flex justify-around items-center mt-4 mb-2 ">
         <button
           onClick={decreaseQuantity}
@@ -34,9 +43,12 @@ export default function Product({ product }) {
           +
         </button>
       </div>
-      <button className="bg-emerald-50 hover:bg-emerald-500 hover:text-white transition-colors duration-500 text-emerald-500 rounded-md px-5 py-2">
+      <button
+        onClick={addToCart}
+        className="bg-emerald-50 hover:bg-emerald-500 hover:text-white transition-colors duration-500 text-emerald-500 rounded-md px-5 py-2"
+      >
         Add to cart
       </button>
     </article>
-  );
+  )
 }
--- pages/success.js
+++ pages/success.js
@@ -1,3 +1,12 @@
+import { useShoppingCart } from 'use-shopping-cart'
+import { useEffect } from 'react'
+
 export default function Success() {
-  return <h1>Your payment was successful. Thank you for your purchase.</h1>;
+  const { clearCart } = useShoppingCart()
+
+  useEffect(() => {
+    clearCart()
+  }, [])
+
+  return <h1>Your payment was successful. Thank you for your purchase.</h1>
 }
